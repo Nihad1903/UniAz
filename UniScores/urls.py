eaa1d0282526028a521cf3d21dfaae51f1dc6b9f
Nihad1903 <nihadtaghiyev1@gmail.com>
2025-06-19 08:36:35 +0400

    Add ads.txt file for AdSense verification

--- UniScores/urls.py
+++ UniScores/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('unichat.urls'))
+    path('', include('unichat.urls')),
+    path("ads.txt", TemplateView.as_view(template_name="ads.txt", content_type="text/plain")),
 ]
